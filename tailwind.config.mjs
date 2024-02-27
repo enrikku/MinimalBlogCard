0d6a0d883e606f102ebea6e95226f8fc340e7a14
enrikku <enric.cancru@gmail.com>
2024-02-27 20:17:42 +0100

    Primer challenge

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			'white': '#FFFFFF',
+			'purple': '#883AE1',
+			'bg-catouse': '#E6D6FC',
+			'text-black': '#20293A',
+			'text-gray': '#6C727F',
+			'design': '#a095af',
+		}
+	},
+	plugins: [],
+}
